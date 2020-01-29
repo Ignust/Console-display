263d63f98422b8e7bbdd591ec3f5a1b9447afb9e
Ignust <andrey.las90@gmail.com>
2020-01-29 15:18:06 +0200

    added target_link_libraries(googlTest ConsoleDisplayLIB)

--- tests/TestConsoleOutput.cpp
+++ tests/TestConsoleOutput.cpp
@@ -1,21 +1,26 @@
 #include <gtest/gtest.h>
 #include <ConsoleOutput.hpp>
 
-/*
+
 class TestConsoleOutput : public ::testing::Test {
     public:
     virtual ~TestConsoleOutput(){}
     protected:
         virtual void SetUp() {
-            mCO = &ConsoleOutput::init();
+            //mCO = &ConsoleOutput::init();
         }
-        ConsoleOutput* mCO;
-
 };
-
+/*
 TEST_F(TestConsoleOutput, printTest) {
     uint8_t** testArray = nullptr;
     EXPECT_ANY_THROW(mCO->print(testArray,1 ,2));
+}*/
+
+TEST(TestConsoleOutput,printTest1){
+
+    uint8_t** testArray = nullptr;
+
+    EXPECT_ANY_THROW(ConsoleOutput::init().print(testArray,1 ,2));
 }
 
-*/
+
